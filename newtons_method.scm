5dc7c9c2aaf8c879820e69ec9cab1ebc44a1b472
tf3 <11424232+TectonicFury@users.noreply.github.com>
2020-12-07 19:19:25 +0545

    الحمد لله learning scheme

--- newtons_method.scm
+++ newtons_method.scm
@@ -1,5 +1,5 @@
 (define (square-root x guess)
-  (if (< (abs (- x (* guess guess))) 0.00001)
+  (if (< (abs (- x (* guess guess))) 0.000001)
     guess
       (square-root x (+ (/ guess 2.0) (/ x (* 2 guess))))
   ))
